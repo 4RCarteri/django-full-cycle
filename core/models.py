c082353d74f985321684e203c6263c22b470da5f
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2024-10-22 16:52:32 -0300

    Refactor Video model to automatically set published_at field on save if is_published is True

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Video(models.Model):
@@ -7,7 +8,7 @@ class Video(models.Model):
     thumbnail = models.ImageField(upload_to='thumbnails/')
     video = models.FileField(upload_to='videos/', verbose_name='Vídeo')
     slug = models.SlugField(max_length=100, unique=True)
-    published_at = models.DateTimeField(verbose_name='Publicado em', editable=False)
+    published_at = models.DateTimeField(verbose_name='Publicado em', editable=False, null=True)
     is_published = models.BooleanField(default=False, verbose_name='Publicado')
     num_likes = models.IntegerField(default=0, verbose_name='Likes', editable=False)
     num_views = models.IntegerField(default=0, verbose_name='Visualizações', editable=False)
@@ -19,6 +20,11 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if self.is_published and not self.published_at:
+            self.published_at = timezone.now()
+        return super().save(*args, **kwargs)
     
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name='Nome')
